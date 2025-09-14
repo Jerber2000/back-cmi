b417c71cf41ffa67b4e946fa82264b60736fe7bb
Jerber <jcintor@miumg.edu.gt>
2025-09-14 17:11:45 -0600

    seguimiento

--- src/index.js
+++ src/index.js
@@ -23,11 +23,15 @@ app.use(helmet());
 // CORS
 
 
+// TEMPORAL - solo para debug
 app.use(cors({
-  origin: 'https://front-cmi-production.up.railway.app',
+  origin: [
+    'https://front-cmi-production.up.railway.app',
+    'http://localhost:4200'  // Para desarrollo
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
 // Logging
